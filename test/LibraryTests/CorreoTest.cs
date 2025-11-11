95dd19f77389d06cc03427f6fe61621173a3b34e
Sebanev15 <sneves@impulso.edu.uy>
2025-11-10 21:02:22 -0300

    Modify IInteraccion and tests from implementations

--- test/LibraryTests/CorreoTest.cs
+++ test/LibraryTests/CorreoTest.cs
@@ -17,7 +17,7 @@ namespace LibraryTests
         {
             randomDate = new DateTime(2025, 1, 1, 10, 30, 0);
             clientito = new Cliente("Juana", "de Arco", "12345678", "juanitayasabes@gmail.com", "F", randomDate);
-            usuarito = new Usuario("josefina", "josefina@gmail.com", "87654321", new GestionCliente());
+            usuarito = new Usuario("josefina", "josefina@gmail.com", "87654321", new GestionUsuario());
             correito = new Correo(randomDate, "holaholahola", clientito, usuarito, true);
         }
 
@@ -29,6 +29,18 @@ namespace LibraryTests
             Assert.That(correito.Cliente, Is.EqualTo(clientito));
             Assert.That(correito.Usuario, Is.EqualTo(usuarito));
             Assert.That(correito.EsEnviado, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void AgregarComentarioTest()
+        {
+            Assert.That(correito.Comentarios.Count, Is.EqualTo(0));
+            correito.AgregarComentario("Primer comentario");
+            Assert.That(correito.Comentarios.Count, Is.EqualTo(1));
+            Assert.That(correito.Comentarios[0], Is.EqualTo("Primer comentario"));
+            correito.AgregarComentario("Segundo comentario");
+            Assert.That(correito.Comentarios.Count, Is.EqualTo(2));
+            Assert.That(correito.Comentarios[1], Is.EqualTo("Segundo comentario"));
         }
     }
 }
--- test/LibraryTests/LlamadaTest.cs
+++ test/LibraryTests/LlamadaTest.cs
@@ -17,7 +17,7 @@ namespace LibraryTest
         {
             randomDate = new DateTime(2025, 1, 1, 10, 30, 0);
             clientito = new Cliente("Juana", "de Arco", "12345678", "juanitayasabes@gmail.com", "F", randomDate);
-            usuarito = new Usuario("josefina", "josefina@gmail.com", "87654321", new GestionCliente());
+            usuarito = new Usuario("josefina", "josefina@gmail.com", "87654321", new GestionUsuario());
             llamadita = new Llamada(randomDate, "holaholahola", clientito, usuarito);
         }
 
@@ -28,6 +28,18 @@ namespace LibraryTest
             Assert.That(llamadita.Tema, Is.EqualTo("holaholahola"));
             Assert.That(llamadita.Cliente, Is.EqualTo(clientito));
             Assert.That(llamadita.Usuario, Is.EqualTo(usuarito));
+        }
+        
+        [Test]
+        public void AgregarComentarioTest()
+        {
+            Assert.That(llamadita.Comentarios.Count, Is.EqualTo(0));
+            llamadita.AgregarComentario("Primer comentario");
+            Assert.That(llamadita.Comentarios.Count, Is.EqualTo(1));
+            Assert.That(llamadita.Comentarios[0], Is.EqualTo("Primer comentario"));
+            llamadita.AgregarComentario("Segundo comentario");
+            Assert.That(llamadita.Comentarios.Count, Is.EqualTo(2));
+            Assert.That(llamadita.Comentarios[1], Is.EqualTo("Segundo comentario"));
         }
     }
 }
